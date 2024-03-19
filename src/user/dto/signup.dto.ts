588cfa21fd390148cd6bc3d6462da04e096d1c53
julie kim <julie.kim.dev@gmail.com>
2024-03-19 15:02:37 +0900

    🗃️  sign up dto create

---
+++ src/user/dto/signup.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class SignUpDto {
+  @IsEmail()
+  @IsNotEmpty({ message: '이메일을 입력해주세요.' })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '비밀번호를 입력해주세요.' })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty({ message: '이름을 입력해주세요.' })
+  name: string;
+
+  @IsString()
+  company: string;
+}
