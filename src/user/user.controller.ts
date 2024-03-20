95d75839f1b5fba7c7054daf4c6ad0236697dc4f
julie kim <julie.kim.dev@gmail.com>
2024-03-20 16:02:37 +0900

    🎨 pr comfirm: dto object update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,6 +7,7 @@ import { LoginDto } from './dto/login.dto';
 
 import { UserInfo } from 'utils/userInfo.decorator';
 import { User } from './entities/user.entity';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
 
 @Controller('user')
 export class UserController {
@@ -14,12 +15,12 @@ export class UserController {
 
     @Post('register')
     async register(@Body() registerDto: RegisterDto) {
-        return await this.userService.register(registerDto.email, registerDto.password, registerDto.name, registerDto.company);
+        return await this.userService.register(registerDto);
     }
 
     @Post('login')
     async login(@Body() loginDto: LoginDto) {
-        return await this.userService.login(loginDto.email, loginDto.password);
+        return await this.userService.login(loginDto);
     }
 
     @UseGuards(AuthGuard('jwt')) // JWT 인증이 된 유저에 한해서 해당 API를 호출하게 해주는 데코레이터
@@ -27,4 +28,18 @@ export class UserController {
     getInfo(@UserInfo() user: User) {
         return { id: user.id, email: user.email, name: user.name, role: user.role };
     }
+
+//     @Get()
+//   @UseGuards(JwtAuthGuard)
+//   async getData(@Req() req) {
+//     const { id, email } = req.user;
+//     const user = await this.userService.findUserById({ id });
+    
+//     // ... 원하는 로직 수행 return
+//   }
+
+    // @Delete('unregister')
+    // unregister(@Body() password: string) {
+    //     return this.po
+    // }
 }
