3b7129bcec1d0e4f64dee57656dd7f315fa5fb5c
choobao <154485888+choobao@users.noreply.github.com>
2024-03-20 14:41:09 +0900

    :sparkles: S3//can upload Image on card

--- src/card/entitis/card.entity.ts
+++ src/card/entitis/card.entity.ts
@@ -1,44 +1,55 @@
-import { BoardMember } from "src/board/entity/boardmembers.entity";
-import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { BoardMember } from 'src/board/entity/boardmembers.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({
-  name: 'card'
+  name: 'cards',
 })
-
 export class Card {
   @PrimaryGeneratedColumn({ unsigned: true })
-  id: number
+  id: number;
 
-  @Column({ type: 'varchar', nullable: false  })
-  title: string
+  @Column({ type: 'varchar', nullable: false })
+  title: string;
 
-  @Column({ type: 'varchar', nullable: false})
-  description: string
+  @Column({ type: 'varchar', nullable: false })
+  description: string;
 
-  @Column({ type: 'varchar', nullable: false})
-  color: string
+  @Column({ type: 'varchar', nullable: true })
+  image: string;
 
-  @Column({ type: 'int', nullable:false})
-  priority: number
+  @Column({ type: 'varchar', nullable: false })
+  color: string;
+
+  @Column({ type: 'int', nullable: false })
+  priority: number;
 
   @CreateDateColumn({ nullable: false })
-  createdAt: Date
+  createdAt: Date;
 
-  @UpdateDateColumn({ nullable: false})
-  updatedAt: Date
+  @UpdateDateColumn({ nullable: false })
+  updatedAt: Date;
 
   @Column({ nullable: true })
-  deadLine: Date
+  deadLine: Date;
 
   @Column()
-  user: number
+  user: number;
 
-  @Column()
-  columnId: number
-// TODO : 상대 테이블에 따라서 수정 요함.
-  @ManyToOne(() => BoardMember, (boardMember) => boardMember.card)
-  boardMember: BoardMember
+  // @Column()
+  // columnId: number;
 
-  @OneToMany(() => CheckLists, (checkLists) => checkLists.card)
-  checkLists: CheckLists
+  // TODO : 상대 테이블에 따라서 수정 요함.
+  // @ManyToOne(() => BoardMember, (boardMember) => boardMember.card)
+  // boardMember: BoardMember
+
+  // @OneToMany(() => CheckLists, (checkLists) => checkLists.card)
+  // checkLists: CheckLists
 }
