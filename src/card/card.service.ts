2c8820a5a6e8bfc32fc3c8978b92928430397521
JSKim <jskim4695@gmail.com>
2024-03-24 23:04:35 +0900

    :adhesive_bandage:: card update bug fix, add card getOne

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -169,17 +169,19 @@ export class CardService {
   // 카드 수정
   async modifyCard(
     user: User,
-    cardId: number,
-    updateCardDto: UpdateCardDto,
-    file: Express.Multer.File,
-  ) {
+    cardId: number, 
+    updateCardDto: UpdateCardDto, 
+    file?: Express.Multer.File
+    ) {
+
     //카드 작성자 확인
     const cardUser = await this.cardRepository.findOne({
       where: { user: user.id },
     });
 
     if (!cardUser) {
-      throw new UnauthorizedException('카드 작업자만 수정할 수 있습니다.');
+      throw new UnauthorizedException('카드 작업자만 수정할 수 있습니다.',
+      );
     }
 
     const card = await this.cardRepository.findOne({
@@ -190,20 +192,27 @@ export class CardService {
       throw new NotFoundException('해당하는 카드가 존재하지 않습니다.');
     }
 
-    //이미 입력된 이미지가 있다면 S3에서 기존 이미지 삭제
-    if (card.image_url !== null) {
-      await this.awsService.DeleteUploadToS3(card.image_url);
-    }
+  let imageUrl = card.image_url;
 
-    //S3에 이미지 업로드, url return
-    const imageName = this.utilsService.getUUID();
-    const ext = file.originalname.split('.').pop();
+  if (file) {
+  //이미 입력된 이미지가 있다면 S3에서 기존 이미지 삭제
+  if (card.image_url !== null) {
+    await this.awsService.DeleteUploadToS3(card.image_url);
+  }
+}
 
-    const imageUrl = await this.awsService.imageUploadToS3(
-      `${imageName}.${ext}`,
-      file,
-      ext,
-    );
+  //S3에 이미지 업로드, url return
+  const imageName = this.utilsService.getUUID();
+  const ext = file? file.originalname.split('.').pop() : null;
+
+  if (ext) {
+  imageUrl = await this.awsService.imageUploadToS3(
+    `${imageName}.${ext}`,
+    file,
+    ext,
+  );
+  }
+  
 
     //DB에 저장
     const uploadCard = await this.cardRepository.save({
@@ -260,122 +269,21 @@ export class CardService {
   }
 
   //카드 상세 조회
+  async findCard(cardId: number) {
+    const card = await this.cardRepository.findOne({
+      where: { id: cardId },
+    });
+
+    if (!card) {
+      throw new NotFoundException('해당하는 카드가 존재하지 않습니다.');
+    }
+
+    return card
+  }  
+
   async getCard(cardId: number) {
     const card = await this.cardRepository.findOne({
       where: { id: cardId },
     });
   }
 }
-
-// constructor(
-//   @InjectRepository(Columns)
-//   private readonly columnRepository: Repository<Columns>,
-//   @InjectRepository(Board)
-//   private readonly boardRepository: Repository<Board>,
-//   @InjectRepository(BoardMember)
-//   private readonly boardMemberRepository: Repository<BoardMember>,
-//   @InjectRepository(Card)
-//   private readonly cardRepository: Repository<Card>,
-//   private readonly utilsService: UtilsService,
-//   private readonly awsService: AwsService,
-// ) {}
-
-// //코드 합친 후 카드생성api에 이미지 입력코드 꼭 넣기!!!!!
-
-// //카드 이미지 입력(수정)
-// async insertMutiformForCard(cardId: number, file: Express.Multer.File) {
-//   const card = await this.cardRepository.findOne({
-//     where: { id: cardId },
-//   });
-
-//   if (!card) {
-//     throw new NotFoundException('해당하는 카드가 존재하지 않습니다.');
-//   }
-
-//   //이미 입력된 이미지가 있다면 S3에서 기존 이미지 삭제
-//   if (card.image !== null) {
-//     await this.awsService.DeleteUploadToS3(card.image);
-//   }
-
-//   //S3에 이미지 업로드, url return
-//   const imageName = this.utilsService.getUUID();
-//   const ext = file.originalname.split('.').pop();
-
-//   const imageUrl = await this.awsService.imageUploadToS3(
-//     `${imageName}.${ext}`,
-//     file,
-//     ext,
-//   );
-
-//   //DB에 저장
-//   const uploadCardDB = await this.cardRepository.update(
-//     { id: card.id },
-//     { image: `${imageName}.${ext}` },
-//   );
-
-//   return { imageUrl };
-// }
-
-// import { Injectable, NotFoundException } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Board } from 'src/board/entity/board.entity';
-// import { BoardMember } from 'src/board/entity/boardmembers.entity';
-// import { Columns } from 'src/column/entities/column.entity';
-// import { Repository } from 'typeorm';
-// import { Card } from './entitis/card.entity';
-// import { UtilsService } from 'utils/utils.service';
-// // import { AwsService } from 'src/aws/aws.service';
-// import { NotFoundError } from 'rxjs';
-
-// @Injectable()
-// export class CardService {
-//   constructor(
-//     @InjectRepository(Columns)
-//     private readonly columnRepository: Repository<Columns>,
-//     @InjectRepository(Board)
-//     private readonly boardRepository: Repository<Board>,
-//     @InjectRepository(BoardMember)
-//     private readonly boardMemberRepository: Repository<BoardMember>,
-//     @InjectRepository(Card)
-//     private readonly cardRepository: Repository<Card>,
-//     private readonly utilsService: UtilsService,
-//     // private readonly awsService: AwsService,
-//   ) {}
-
-//   //코드 합친 후 카드생성api에 이미지 입력코드 꼭 넣기!!!!!
-
-//   //카드 이미지 입력(수정)
-//   async insertMutiformForCard(cardId: number, file: Express.Multer.File) {
-//     const card = await this.cardRepository.findOne({
-//       where: { id: cardId },
-//     });
-
-//     if (!card) {
-//       throw new NotFoundException('해당하는 카드가 존재하지 않습니다.');
-//     }
-
-//     //이미 입력된 이미지가 있다면 S3에서 기존 이미지 삭제
-//     if (card.image !== null) {
-//       // await this.awsService.DeleteUploadToS3(card.image);
-//     }
-
-//     //S3에 이미지 업로드, url return
-//     const imageName = this.utilsService.getUUID();
-//     const ext = file.originalname.split('.').pop();
-
-//     // const imageUrl = await this.awsService.imageUploadToS3(
-//     //   `${imageName}.${ext}`,
-//     //   file,
-//     //   ext,
-//     // );
-
-//     //DB에 저장
-//     const uploadCardDB = await this.cardRepository.update(
-//       { id: card.id },
-//       { image: `${imageName}.${ext}` },
-//     );
-
-//     // return { imageUrl };
-//   }
-
-// }
